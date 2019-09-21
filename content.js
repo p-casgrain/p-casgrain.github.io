6017105d842db1db22351019edfd102074573e45
hotdog2000 <pcasgrain@gmail.com>
2019-09-20 23:59:59 -0400

    First Commit

---
+++ content.js
@@ -0,0 +1,27 @@
+// Define content here as strings with Markdown code within them
+
+// About Text
+aboutText = 
+'#### About Me  \n' +
+'I am currently a quantitative researcher working on algorithmic execution at [Citadel Asset Management](https://www.citadel.com/) in New York City.  \n\n' +
+'I completed my PhD in 2018 in the [Department of Statistical Sciences](http://www.utstat.toronto.edu/) at the University of Toronto, under the supervision of [Sebastian Jaimungal](http://sebastian.statistics.utoronto.ca/).';
+
+// Contact Info
+emailE = 'mail.utoronto.ca'
+emailE = ('pcasgrain' + '@' + emailE)
+
+contactText = 
+'#### Links and Contact \n' +  
+`_Email:_ [${emailE}](mailto:${emailE})   \n` +
+'_Twitter_: [@phil_casgrain](https://twitter.com/phil_casgrain)   \n' +
+'[My Github Profile](https://github.com/p-casgrain/)  \n' +
+'[My LinkedIn Profile](https://www.linkedin.com/in/philippe-casgrain-aab169134/)   \n' +
+'[My Google Scholar Profile](https://scholar.google.com/citations?user=g1lLt3sAAAAJ&hl=en)  \n'
+
+// Research
+researchInterestsTxt =  '### Research \n' +
+'My current research interests span Mathematical Finance, Statistics and Computer Science. My dissertation was focused on problems in _mean-field games_, _stochastic control_ and _statistical machine learning_ stemming from algorithmic trading. My more recent work uses insights and mathematical tools from _stochastic control_ to solve problems related to _stochastic optimization_ and _multi-agent reinforcement learning_. \n\n My PhD thesis, _Algorithmic Trading with Latent Models and Mean-Field Games_, can be found [here](https://tspace.library.utoronto.ca/bitstream/1807/95757/3/Casgrain_Philippe_201906_PhD_thesis.pdf). My most recent work can be found on [my arXiv author page](https://arxiv.org/search/?query=Casgrain&searchtype=all) or below. I am always eager to find new people to work with, please contact me if you are interested in collaborating.'
+	
+
+
+//
