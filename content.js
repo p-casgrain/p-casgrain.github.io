30fa4a943974c181cdbbe3fc45030e4ac6f5c4ad
hotdog2000 <pcasgrain@gmail.com>
2020-04-28 18:09:59 -0400

    Removed newline in about

--- content.js
+++ content.js
@@ -3,7 +3,7 @@
 // About Text
 aboutText = 
 '#### About Me  \n' +
-'I am currently a Postdoctoral research fellow in Zürich, Switzerland, jointly appointed between [ETH Zürich](https://math.ethz.ch/) and [Princeton University](https://orfe.princeton.edu/). \n\n' + 
+'I am currently a Postdoctoral research fellow in Zürich, Switzerland, jointly appointed between [ETH Zürich](https://math.ethz.ch/) and [Princeton University](https://orfe.princeton.edu/). ' + 
 'Previously, I was a quantitative researcher working on algorithmic trading and execution at [Citadel Asset Management](https://www.citadel.com/) in New York City.  \n\n' +
 'I completed my PhD in the [Department of Statistical Sciences](http://www.utstat.toronto.edu/) at the University of Toronto in 2018, under the supervision of [Sebastian Jaimungal](http://sebastian.statistics.utoronto.ca/). During my PhD, I was a graduate research fellow in the [Roy lab](http://danroy.org/) at the [Vector Institute for Artificial Intelligence](https://vectorinstitute.ai/).';
 
