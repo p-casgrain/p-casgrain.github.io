1ccbde929877751cea6e6628b0e0e7fce53cdf0a
hotdog2000 <pcasgrain@gmail.com>
2020-04-28 18:20:54 -0400

    Update content.js

--- content.js
+++ content.js
@@ -22,8 +22,8 @@ contactText =
 // Research
 researchInterestsTxt =  
 '### Research \n' +
-'My current research interests span Mathematical Finance, Statistics and Computer Science. My dissertation was focused on problems in _mean-field games_, _stochastic control_ and _statistical machine learning_ stemming from algorithmic trading. My more recent work uses insights and mathematical tools from _stochastic control_ to solve problems related to _stochastic optimization_ and _multi-agent reinforcement learning_. \n\n' +
-'My PhD thesis, _Algorithmic Trading with Latent Models and Mean-Field Games_, can be found [here](https://tspace.library.utoronto.ca/bitstream/1807/95757/3/Casgrain_Philippe_201906_PhD_thesis.pdf). My most recent work can be found on [my arXiv author page](https://arxiv.org/search/?query=Casgrain&searchtype=all) or below. I am always eager to find new people to work with, please contact me if you are interested in collaborating.'
+'My current research interests span Mathematical Finance, Statistics and Computer Science. My dissertation was focused on problems in *mean-field games*, *stochastic control* and *statistical machine learning* stemming from algorithmic trading. My more recent work uses insights and mathematical tools from *stochastic control* to solve problems related to *stochastic optimization* and *multi-agent reinforcement learning*. \n\n' +
+'My PhD thesis, *Algorithmic Trading with Latent Models and Mean-Field Games*, can be found [here](https://tspace.library.utoronto.ca/bitstream/1807/95757/3/Casgrain_Philippe_201906_PhD_thesis.pdf). My most recent work can be found on [my arXiv author page](https://arxiv.org/search/?query=Casgrain&searchtype=all) or below. I am always eager to find new people to work with, please contact me if you are interested in collaborating.'
 	
 
 
